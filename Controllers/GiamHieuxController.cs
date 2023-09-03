3c977921fb52aeabd2aeaf81c68d8a103d2366a5
WIN-NDJS73S1KUT\NguyenTrungThanh <tytytititity@gmail.com>
2023-09-03 12:57:42 +0700

    update template layout

---
+++ Controllers/GiamHieuxController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using QuanLyDaiHoc.Models;
+
+namespace QuanLyDaiHoc.Controllers
+{
+    public class GiamHieuxController : Controller
+    {
+        private readonly QuanLyDaiHoc2Context _context;
+        public GiamHieuxController(QuanLyDaiHoc2Context context)
+        {
+            _context = context;
+        }
+        public async Task <IActionResult> Index()
+        {
+            return _context.GiamHieus != null ?
+            View(await _context.GiamHieus.ToListAsync()) :
+            Problem("Entity set ' QuanLyDaiHoc2Context.GiamHieux'is null. ");
+        }
+    }
+}
